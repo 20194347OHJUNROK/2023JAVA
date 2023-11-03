1744ba06797c9021b605f2aca33de16510cdb8dc
20194347오준록 <148172326+20194347OHJUNROK@users.noreply.github.com>
2023-11-03 12:56:06 +0900

    예제 6_6절 실습입니다

---
+++ Chapter6_6/C.java
@@ -0,0 +1,14 @@
+package Chapter6_6;
+
+public class C {
+	public C() {
+		A a = new A();
+		a.field1 = 1;   
+		//a.field2 = 1;   
+		//a.field3 = 1;   
+		
+		a.method1();   
+		//a.method2();   
+		//a.method3();   
+	}
+}
